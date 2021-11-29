d1339d3c5de1e9b23c68079986d83a5e20cc3432
Matheus <matheusaraujo.dev@gmail.com>
2021-11-29 10:18:03 -0300

    Updates

---
+++ precache-manifest.4a075d3f7385a3ef1ae79b2e7648a109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b21b20f851c42d96fc909d8e155a46",
+    "url": "/app-toot-name/index.html"
+  },
+  {
+    "revision": "7c82ccb3d9f75cd153ec",
+    "url": "/app-toot-name/static/css/main.d04ce568.chunk.css"
+  },
+  {
+    "revision": "0dc03073afddeef1a423",
+    "url": "/app-toot-name/static/js/2.572746f5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/app-toot-name/static/js/2.572746f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c82ccb3d9f75cd153ec",
+    "url": "/app-toot-name/static/js/main.94e00a16.chunk.js"
+  },
+  {
+    "revision": "1b1943e62e59b0082ef1",
+    "url": "/app-toot-name/static/js/runtime-main.946983d5.js"
+  }
+]);
